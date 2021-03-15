7349a03147904d48fbbdbfe8c1b9ee4a5cb5030a
heshaojun <keepword_heshaojun@hotmail.com>
2021-03-15 16:49:17 +0800

    Update NewestArticleService.groovy

--- BlogWebSite/src/main/groovy/cn/codejavahand/service/NewestArticleService.groovy
+++ BlogWebSite/src/main/groovy/cn/codejavahand/service/NewestArticleService.groovy
@@ -5,11 +5,8 @@ import cn.codejavahand.common.RestResp
 import cn.codejavahand.config.SysConfig
 import cn.codejavahand.dao.IArticleIdRepo
 import cn.codejavahand.dao.IArticleInfoRepo
-import cn.codejavahand.dao.ICacheCleaner
 import cn.codejavahand.dao.po.ArticleInfoPo
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.cache.annotation.CacheEvict
-import org.springframework.cache.annotation.Cacheable
 import org.springframework.stereotype.Service
 
 import java.text.SimpleDateFormat
@@ -21,7 +18,7 @@ import java.text.SimpleDateFormat
  * @Description TODO
  */
 @Service
-class NewestArticleService implements ICacheCleaner {
+class NewestArticleService {
     @Autowired
     private SysConfig sysConfig
     @Autowired
@@ -32,7 +29,25 @@ class NewestArticleService implements ICacheCleaner {
     RestResp doService() {
         RestResp resp = [code: 300, msg: "fail"] as RestResp
         try {
-            List<ArticleInfoPo> articleInfoPoTemList = getNewestArticles()
+            List<String> articleIdList = articleIdRepo.getAllArticleList()
+            List<ArticleInfoPo> articleInfoPoList = new ArrayList<>()
+            List<ArticleInfoPo> articleInfoPoTemList = new ArrayList<>()
+            List<NewestArticleBo> data = new ArrayList<>()
+            articleIdList.each {
+                ArticleInfoPo articleInfoPo = articleInfoRepo.getArticleInfoById("$it")
+                if (articleInfoPo) {
+                    articleInfoPoList.add articleInfoPo
+                }
+            }
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+            articleInfoPoList.sort { t1, t2 -> dateFormat.parse(t2.time).getTime() - dateFormat.parse(t1.time).getTime() }
+            if (articleInfoPoList) {
+                if (articleInfoPoList.size() <= sysConfig.newestArticleListNum) {
+                    articleInfoPoTemList = articleInfoPoList
+                } else {
+                    articleInfoPoTemList = articleInfoPoList.subList(0, sysConfig.newestArticleListNum)
+                }
+            }
             articleInfoPoTemList.each {
                 data.add([title: it.title, id: it.id] as NewestArticleBo)
             }
@@ -43,33 +58,4 @@ class NewestArticleService implements ICacheCleaner {
         resp
     }
 
-    @Cacheable(cacheNames = ["NewestArticleBoList"])
-    List<NewestArticleBo> getNewestArticles() {
-        List<String> articleIdList = articleIdRepo.getAllArticleList()
-        List<ArticleInfoPo> articleInfoPoList = new ArrayList<>()
-        List<ArticleInfoPo> articleInfoPoTemList = new ArrayList<>()
-        List<NewestArticleBo> data = new ArrayList<>()
-        articleIdList.each {
-            ArticleInfoPo articleInfoPo = articleInfoRepo.getArticleInfoById("$it")
-            if (articleInfoPo) {
-                articleInfoPoList.add articleInfoPo
-            }
-        }
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
-        articleInfoPoList.sort { t1, t2 -> dateFormat.parse(t2.time).getTime() - dateFormat.parse(t1.time).getTime() }
-        if (articleInfoPoList) {
-            if (articleInfoPoList.size() <= sysConfig.newestArticleListNum) {
-                articleInfoPoTemList = articleInfoPoList
-            } else {
-                articleInfoPoTemList = articleInfoPoList.subList(0, sysConfig.newestArticleListNum)
-            }
-        }
-        articleInfoPoTemList
-    }
-
-    @CacheEvict(cacheNames = ["NewestArticleBoList"])
-    @Override
-    void cleanCache() {
-
-    }
 }

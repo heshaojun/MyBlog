43b729858ff7090ab10500d146ed35abd636ad2a
heath <heath@heathdeMacBook-Pro.local>
2021-04-15 10:10:26 +0800

    提交部分代码

--- BlogEditor/renderer/js/editor.js
+++ BlogEditor/renderer/js/editor.js
@@ -28,8 +28,8 @@ $(function () {
     $('#tables-area').tabs({
         border: false,
         fit: true,
-        onBeforeClose: function (title, id) {
-            return closeSafely(title, id);
+        onBeforeClose: function (title, index) {
+            return closeSafely(title, index);
         },
         onLoad: function (panel) {
             loadData(panel)
@@ -155,6 +155,7 @@ function articleRender(articleId, context) {
     $(".article-show-area[article_id='" + articleId + "']").html(html);
 }
 
+//文章内容变动触发渲染
 function articleContextChanged(selector) {
     let articleId = $(selector).attr("article_id");
     let context = $(selector).val();
@@ -167,29 +168,66 @@ function ifArticleChanged(title, id) {
 }
 
 //保存修改
-function saveChange(selector, id) {
-    let articleId = id;
-    if (selector) {
-        articleId = $(selector).attr("article_id");
-    }
-    let panel;
-    let panels = $('#tables-area').tabs('tabs');
-    for (let p in panels) {
-        if ($(p).attr("id") == articleId) {
-            panel = p;
-            break;
-        }
+function saveChange() {
+    let panel = $('#tables-area').tabs("getSelected");
+    let articleId = $(panel).attr("id");
+    let articleDataInPage = getDataFromTable(panel);
+    let articleInDisc = ipcRenderer.sendSync("read-article", articleId);
+    console.log("---------------------")
+    console.log(articleInDisc);
+    console.log(articleDataInPage);
+    if (ifArticleEqual(articleDataInPage, articleInDisc)) {
+        alert("内容没有变更");
     }
 }
 
+function ifArticleEqual(article1, article2) {
+    debugger
+    if (article1["title"] != article2["title"]) return false;
+    if (article1["summery"] != article2["summery"]) return false;
+    if (article1["type"] != article2["type"]) return false;
+    if (article1["time"] != article2["time"]) return false;
+    if (article1["id"] != article2["id"]) return false;
+    if (article1["context"] != article2["context"]) return false;
+    if (article1["articleLabel"] != article2["articleLabel"]) return false;
+    if (article1["classifyLabels"].toString() != article2["classifyLabels"].toString()) return false;
+    return true
+}
+
+//获取tab页中文章内容
 function getDataFromTable(panel) {
     let articleId = $(panel).attr('id');
+    let articleTitle = $(panel).find(".article-title[article_id='" + articleId + "']");
+    let articleType = $(panel).find(".article-type[article_id='" + articleId + "']");
+    let articleCreateTime = $(panel).find(".article-create-time[article_id='" + articleId + "']");
+    let articleLabel = $(panel).find(".article-label[article_id='" + articleId + "']");
+    let articleClassify = $(panel).find(".article-classify[article_id='" + articleId + "'] .article-classify-text");
+    let articleSummary = $(panel).find(".article-summary[article_id='" + articleId + "']");
+    let articleDetail = $(panel).find(".article-detail[article_id='" + articleId + "']");
+    let classifyLabels = [];
+    $($(panel).find(".article-classify[article_id='" + articleId + "'] .article-classify-text")).each(function () {
+        classifyLabels.push($(this).text());
+    });
+
+    let articleData = {
+        "title": $(articleTitle).val(),
+        "summery": $(articleSummary).val(),
+        "type": $(articleType).val(),
+        "time": $(articleCreateTime).val(),
+        "id": articleId,
+        "context": $(articleDetail).val(),
+        "articleLabel": $(articleLabel).val(),
+        "classifyLabels": classifyLabels
+    }
+    return articleData;
 
 }
 
-function closeSafely(title, id) {
-    if (ifArticleChanged(title, id)) {
-
+function closeSafely(title, index) {
+    if (ifArticleChanged(title, index)) {
+        let panel = $('#tables-area').tabs('getTab', index);
+        let articleData = getDataFromTable(panel);
+        console.log("获取页面数据-------")
     }
     return true
 }
